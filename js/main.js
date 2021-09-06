06b1df9039d9602735254b823c79d6b6013bb18d
MundumCustos <russiannik11@gmail.com>
2021-09-06 12:45:00 +0300

    final

--- js/main.js
+++ js/main.js
@@ -10,3 +10,36 @@ $(function(){
       });
 
 });
+
+
+//  Таймер
+
+let time = 1800;
+
+const countDownEl = document.getElementById('countdown');
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+  seconds = seconds < 10 ? "0" + seconds: seconds;
+  countDownEl.innerHTML = `${minutes}:${seconds}`;
+  time--;
+}
+
+// очистка поля при вводе букв а не цифр
+
+document.querySelector('.myValidate').addEventListener('keyup', function(){
+  this.value = this.value.replace(/[^\d]/g, '');
+});
+
+// заседлиный скролинг к форме заказа 
+
+function slowScroll(id) { 
+  var offset = 0;
+  $('html, body').animate({ 
+       scrollTop: $(id).offset().top - offset 
+  }, 1000);
+  return false; 
+} 
